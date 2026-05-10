e44f67692680d7e5e83e66e3fb3c6b0fa52b6b96
Damir Vafin <damirvafinnailevich@gmail.com>
2026-05-10 13:56:12 +0300

    feat: добавить контроллер для ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,48 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.constant.HttpHeadersConstants;
+import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/requests")
+@RequestMapping("/requests")
+@RequiredArgsConstructor
 public class ItemRequestController {
+
+    private final ItemRequestService requestService;
+
+    @PostMapping
+    public ItemRequestDto create(
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId,
+            @Valid @RequestBody ItemRequestCreateDto createDto
+    ) {
+        return requestService.create(userId, createDto);
+    }
+
+    @GetMapping
+    public List<ItemRequestDto> findAllByUser(
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
+    ) {
+        return requestService.findAllByUser(userId);
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDto> findAllOther(
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
+    ) {
+        return requestService.findAllOther(userId);
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestDto findById(
+            @PathVariable Long requestId,
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
+    ) {
+        return requestService.findById(requestId, userId);
+    }
 }
