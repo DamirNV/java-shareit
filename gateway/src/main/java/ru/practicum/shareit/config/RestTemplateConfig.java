ba82610bf42919620c533c4957a521743f6573cc
Damir Vafin <damirvafinnailevich@gmail.com>
2026-05-11 12:47:09 +0300

    fix: добавить httpclient5 с правильной версией и обновить конфигурацию RestTemplate

--- gateway/src/main/java/ru/practicum/shareit/config/RestTemplateConfig.java
+++ gateway/src/main/java/ru/practicum/shareit/config/RestTemplateConfig.java
@@ -1,7 +1,7 @@
 package ru.practicum.shareit.config;
 
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -12,7 +12,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        HttpClient httpClient = HttpClientBuilder.create().build();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(factory);
     }
