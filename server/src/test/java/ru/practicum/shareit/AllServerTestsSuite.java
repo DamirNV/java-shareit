1453e6f398a1221a05a3ba94bcff957088256af5
Damir Vafin <damirvafinnailevich@gmail.com>
2026-05-11 15:48:03 +0300

    test: добавить Suite для запуска всех тестов в server и gateway

---
+++ server/src/test/java/ru/practicum/shareit/AllServerTestsSuite.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Все тесты Server ShareIt")
+@IncludeEngines("junit-jupiter")
+@SelectPackages({
+        "ru.practicum.shareit.user",
+        "ru.practicum.shareit.item",
+        "ru.practicum.shareit.booking",
+        "ru.practicum.shareit.request"
+})
+public class AllServerTestsSuite {
+}
